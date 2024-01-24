a8897ddc4b4f60651ae6078d46b159427611a279
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-24 07:25:24 -0500

    fix

--- frontend/netlify/functions/api.ts
+++ frontend/netlify/functions/api.ts
@@ -1,73 +1,14 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
-import fs from "node:fs";
-import path from "node:path";
 
+import {createServer} from "../../server";
 const api = express();
 
-const isProd = process.env.NODE_ENV === "production"
-let   root = process.cwd()
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
-const __dirname = require('path').dirname(__filename);
-const resolve = (p) => path.resolve(__dirname, p);
-
-const indexProd = isProd
-  ? fs.readFileSync(resolve("dist/client/index.html"), "utf-8")
-  : "";
-
-const manifest = isProd
-  ? JSON.parse(
-    fs.readFileSync(resolve("dist/client/ssr-manifest.json"), "utf-8")
-  )
-  : {};
-
-
-let vite;
-if (!isProd) {
-  vite = await (
-    await import("vite")
-  )
-  api.use(vite.middlewares);
-} else {
-  api.use((await import("compression")).default());
-  api.use(
-    "/",
-    (await import("serve-static")).default(resolve("dist/client"), {
-      index: false,
-    })
-  );
-}
-
-
-api.use("*", async (req, res) => {
-    try {
-      const url = req.originalUrl;
-
-      let template, render;
-      if (!isProd) {
-        template = fs.readFileSync(resolve("index.html"), "utf-8");
-        template = await vite.transformIndexHtml(url, template);
-        console.log("ENTRA EN DEV");
-        render = (await vite.ssrLoadModule("/src/entry-server.js")).render;
-      } else {
-        template = indexProd;
-        console.log("ENTRA ACA EN PROD");
-        render = (await import("../../dist/server/entry-server.js")).render;
-      }
-
-      const [appHtml, preloadLinks] = await render(url, manifest);
-
-      const html = template
-        .replace(`<!--preload-links-->`, preloadLinks)
-        .replace(`<!--app-html-->`, appHtml);
-
-      res.status(200).set({ "Content-Type": "text/html" }).end(html);
-    } catch (e) {
-      vite && vite.ssrFixStacktrace(e);
-      console.log(e.stack);
-      res.status(500).end(e.stack);
-    }
-  });
+api.use("/api/", router);
+createServer()
 
 export const handler = serverless(api);
 
