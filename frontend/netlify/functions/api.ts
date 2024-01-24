e3cf04fc8a0e04a156240864f00cf127757ff461
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-24 11:14:27 -0500

    test

--- frontend/netlify/functions/api.ts
+++ frontend/netlify/functions/api.ts
@@ -1,13 +1,59 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
+import express, { Router } from 'express'
+import serverless from 'serverless-http'
+import fs from 'node:fs'
+import path from 'node:path'
 
-import {createServer} from "../../server";
-const api = express();
+import { createServer } from '../../server'
+const api = express()
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+const router = Router()
+const __dirname = require('path').dirname(__filename)
+const resolve = (p) => path.resolve(__dirname, p)
 
-api.use("/", router);
+router.get('/hello', (req, res) => res.send('Hello World!'))
 
-export const handler = serverless(api);
+api.use('/', router)
 
+api.use((await import("compression")).default());
+api.use(
+  "/",
+  (await import("serve-static")).default(resolve("dist/client"), {
+    index: false,
+  })
+);
+
+api.use('*', async (req, res) => {
+  
+  /**
+   * @type {import('vite').ViteDevServer}
+   */
+  let vite;
+  vite = await (
+    await import("vite"))
+  try {
+    
+    const url = req.originalUrl
+
+
+
+    let template, render
+    const indexProd = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+
+    template = indexProd
+    render = (await import('./dist/server/entry-server.js')).render
+
+    const manifest = JSON.parse(fs.readFileSync(resolve('dist/client/ssr-manifest.json'), 'utf-8'))
+
+    const [appHtml, preloadLinks] = await render(url, manifest)
+
+    const html = template.replace(`<!--preload-links-->`, preloadLinks).replace(`<!--app-html-->`, appHtml)
+
+    res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+  } catch (e) {
+    vite && vite.ssrFixStacktrace(e)
+    console.log(e.stack)
+    res.status(500).end(e.stack)
+  }
+})
+
+export const handler = serverless(api)
