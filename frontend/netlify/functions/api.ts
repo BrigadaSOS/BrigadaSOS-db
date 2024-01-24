32d82efdd90336f420ba9a3502c8f0b3d7aa7791
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-24 09:29:59 -0500

    test

--- frontend/netlify/functions/api.ts
+++ frontend/netlify/functions/api.ts
@@ -7,8 +7,7 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
-createServer()
+api.use("/", router);
 
 export const handler = serverless(api);
 
