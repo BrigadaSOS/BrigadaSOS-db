879b5b3c22413e80d643cc1b4ebcda7c9ac58e57
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-25 01:02:33 -0500

    test

--- frontend/netlify/functions/server.js
+++ frontend/netlify/functions/server.js
@@ -47,11 +47,7 @@ function listSubdirectories(dir, depth, currentLevel = 0) {
 
 export async function createServer(root = process.cwd(), isProd = process.env.NODE_ENV !== 'production', hmrPort) {
   console.log('Current directory: ' + process.cwd());
-  // ./frontend/netlify/
-  //listDirectoryContents('../../');
-  listSubdirectories('../../', 10);
-
-  
+    
   const indexProd = isProd ? fs.readFileSync(('../client/index.html'), 'utf-8') : ''
 
   const manifest = isProd ? JSON.parse(fs.readFileSync(('/client/.vite/ssr-manifest.json'), 'utf-8')) : {}
