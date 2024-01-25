401c48314a985290cb1ac5ad0d68f9a42307b024
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-25 10:04:36 -0500

    fix

--- frontend/netlify/functions/server.js
+++ frontend/netlify/functions/server.js
@@ -49,7 +49,7 @@ export async function createServer(root = process.cwd(), isProd = process.env.NO
   console.log('Current directory: ' + process.cwd());
   // ./frontend/netlify/
   //listDirectoryContents('../../');
-  listDirectoryContents('.');
+  listDirectoryContents('./frontend');
 
   
   const indexProd = isProd ? fs.readFileSync(('../client/index.html'), 'utf-8') : ''
