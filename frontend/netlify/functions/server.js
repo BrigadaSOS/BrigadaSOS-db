a93ddedbd603b4329984128f7fce9db0aaa09c0e
Jonathan <Jonathan.197ariza@gmail.com>
2024-01-25 00:33:30 -0500

    test

--- frontend/netlify/functions/server.js
+++ frontend/netlify/functions/server.js
@@ -22,7 +22,7 @@ function listDirectoryContents(directory) {
 
 export async function createServer(root = process.cwd(), isProd = process.env.NODE_ENV !== 'production', hmrPort) {
   console.log('Current directory: ' + process.cwd());
-  listDirectoryContents('.');
+  listDirectoryContents('./frontend');
 
   
   const indexProd = isProd ? fs.readFileSync(('../client/index.html'), 'utf-8') : ''
